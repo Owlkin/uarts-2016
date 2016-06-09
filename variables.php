068b691e01bd3589771ae5b408c4f26d54326397
Augustus Wilder <owlg5@hotmail.com>
2016-06-09 00:11:27 +0000

    Variables exercises

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+
+    $firstName = "Augustus";
+    $lastName = "Wilder";
+    
+    echo $firstName ." ". $lastName;
+
+    echo "<br>";
+    
+    $firstVar = 4;
+    $secondVar = 6;
+
+    echo $firstVar + $secondVar;
+    
+    echo "<br>";
+    
+    if ($firstVar < 10) {
+        echo $firstVar ." ". "is less than 10";
+    } else if ($firstVar > 10) {
+        echo $firstVar ." ". "is greater than 10";
+    } else if ($firstVar == 10) {
+        echo $firstVar ." ". "is equal to 10";
+    }
+    
+?>
