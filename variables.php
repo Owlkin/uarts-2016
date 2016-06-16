4ba75dce371dcf229f6477220156f3dd1c112352
Augustus Wilder <owlg5@hotmail.com>
2016-06-16 00:03:19 +0000

    Mid-class commit

--- variables.php
+++ variables.php
@@ -12,7 +12,7 @@
 
     echo $firstVar + $secondVar;
     
-    echo "<br>";
+    echo "<hr>";
     
     if ($firstVar < 10) {
         echo $firstVar ." ". "is less than 10";
@@ -22,7 +22,7 @@
         echo $firstVar ." ". "is equal to 10";
     }
     
-    echo "<br>";
+    echo "<hr>";
     
     $brushNum = 0;
     $currentCost = 0;
@@ -36,6 +36,61 @@
         }
     }
     
+    echo "<hr>";
     
+    $brushNum2 = 100;
+    $brushCost2 = 3;
+    $currentCost2 = $brushNum2 * $brushCost2;
+    
+    echo "$currentCost2";
+    echo "<br>";
+    
+    do {
+        $currentCost2 = $currentCost2 - 3;
+        $brushNum2 = $brushNum2 - 1;
+        if ($currentCost2 % 10 === 0) {
+            echo $currentCost2;
+            echo "<br>";
+        }
+    }
+    while ($brushNum2 > 0);
+    
+    echo "<hr>";
+    
+    for ($i=1; $i <= 10; $i++){
+        echo "$i";
+        echo "<br>";
+    }
+    
+    echo "<hr>";
+    
+    $states=array("Pennsylvania", "Arkansas", "Massachusets", "Texas", "California", "Montana");
+    $statesLength=count($states);
+    echo "<select>";
+    for ($i=0; $i < $statesLength; $i++){
+        echo "<option value='" . $states[$i] . "'>" . $states[$i] . "</option>";
+    }
+    echo "</select>";
+    
+    echo "<hr>";
+    
+    $statesAssoc=array("Pennsylvania"=>"Harrisburg", "Maine"=>"Augusta", "Hawaii"=>"Honolulu", "Texas"=>"Austin");
+    foreach ($statesAssoc as $x => $x_value){
+        echo "The capitol of " . $x . " is " . $x_value;
+        echo "<br>";
+    }
+    
+    echo "<hr>";
+    
+    $books = array (
+        array("The Hitchhiker's Guide to the Galaxy", "10", "Douglas Adams");
+        array("The Lord of the Rings", "9", "J. R. R. Tolkien");
+        array("The Martian", "13", "Andy Weir");
+        array("The Color of Magic", "8", "Terry Pratchett");
+        array("I, Robot", "6", "Isaac Asimov");
+        array("The Way of Kings", "11", "Brandon Sanderson");
+    );
+    
+    foreach ()
     
 ?>
